925128b08ab5f3c056ef40aaed6839010ae241fa
Nicolas Grekas <nicolas.grekas@gmail.com>
2008-02-03 11:16:13 +0000

    - fixed autoload marker substitution

--- class/u.php
+++ class/u.php
@@ -86,7 +86,7 @@ class u
 
 	static function substr($s, $start, $len = INF)
 	{
-		$s = self::getGraphemeClusterArray($s);
+		$s =& self::getGraphemeClusterArray($s);
 		$s = array_slice($s, $start, INF === $len ? PHP_INT_MAX : $len);
 		return implode('', $s);
 	}
@@ -143,7 +143,7 @@ class u
 
 					if ($cut && $wLen > $width)
 					{
-						$w = self::getGraphemeClusterArray($w);
+						$w =& self::getGraphemeClusterArray($w);
 
 						do
 						{
@@ -184,7 +184,7 @@ class u
 	static function count_chars($s, $mode = 1)
 	{
 		if (1 != $mode && 3 != $mode) trigger_error('u::count_chars(): allowed $mode are 1 or 3', E_USER_ERROR);
-		$s = self::getGraphemeClusterArray($s);
+		$s =& self::getGraphemeClusterArray($s);
 		$s = array_count_values($s);
 		return 1 == $mode ? $s[0] : implode('', $s[0]);
 	}
@@ -272,7 +272,7 @@ class u
 
 	static function str_shuffle($s)
 	{
-		$s = self::getGraphemeClusterArray($s);
+		$s =& self::getGraphemeClusterArray($s);
 		shuffle($s);
 		return implode('', $s);
 	}
@@ -282,7 +282,7 @@ class u
 		$len = (int) $len;
 		if ($len < 1) return str_split($s, $len);
 
-		$s = self::getGraphemeClusterArray($s);
+		$s =& self::getGraphemeClusterArray($s);
 		if (1 === $len) return $s;
 
 		$a = array();
@@ -342,7 +342,7 @@ class u
 
 	static function strrev($s)
 	{
-		$s = self::getGraphemeClusterArray($s);
+		$s =& self::getGraphemeClusterArray($s);
 		return implode('', array_reverse($s));
 	}
 
@@ -356,8 +356,8 @@ class u
 	{
 		if (INF !== $to)
 		{
-			$from = self::getGraphemeClusterArray($from);
-			$to   = self::getGraphemeClusterArray($to);
+			$from =& self::getGraphemeClusterArray($from);
+			$to   =& self::getGraphemeClusterArray($to);
 
 			$a = count($from);
 			$b = count($to);
@@ -384,8 +384,8 @@ class u
 
 	static function substr_replace($s, $replace, $start, $len = INF)
 	{
-		$s       = self::getGraphemeClusterArray($s);
-		$replace = self::getGraphemeClusterArray($replace);
+		$s       =& self::getGraphemeClusterArray($s);
+		$replace =& self::getGraphemeClusterArray($replace);
 
 		if (INF === $len) $len = count($s);
 
@@ -409,7 +409,7 @@ class u
 	// (CRLF|([ZWNJ-ZWJ]|T+|L*(LV?V+|LV|LVT)T*|L+|[^Control])[Extend]*|[Control])
 	const GRAPHEME_CLUSTER_RX = '/(?:\r\n|(?:[ -~\x{200C}\x{200D}]|[ᆨ-ᇹ]+|[ᄀ-ᅟ]*(?:[가개갸걔거게겨계고과괘괴교구궈궤귀규그긔기까깨꺄꺠꺼께껴꼐꼬꽈꽤꾀꾜꾸꿔꿰뀌뀨끄끠끼나내냐냬너네녀녜노놔놰뇌뇨누눠눼뉘뉴느늬니다대댜댸더데뎌뎨도돠돼되됴두둬뒈뒤듀드듸디따때땨떄떠떼뗘뗴또똬뙈뙤뚀뚜뚸뛔뛰뜌뜨띄띠라래랴럐러레려례로롸뢔뢰료루뤄뤠뤼류르릐리마매먀먜머메며몌모뫄뫠뫼묘무뭐뭬뮈뮤므믜미바배뱌뱨버베벼볘보봐봬뵈뵤부붜붸뷔뷰브븨비빠빼뺘뺴뻐뻬뼈뼤뽀뽜뽸뾔뾰뿌뿨쀄쀠쀼쁘쁴삐사새샤섀서세셔셰소솨쇄쇠쇼수숴쉐쉬슈스싀시싸쌔쌰썌써쎄쎠쎼쏘쏴쐐쐬쑈쑤쒀쒜쒸쓔쓰씌씨아애야얘어에여예오와왜외요우워웨위유으의이자재쟈쟤저제져졔조좌좨죄죠주줘줴쥐쥬즈즤지짜째쨔쨰쩌쩨쪄쪠쪼쫘쫴쬐쬬쭈쭤쮀쮜쮸쯔쯰찌차채챠챼처체쳐쳬초촤쵀최쵸추춰췌취츄츠츼치카캐캬컈커케켜켸코콰쾌쾨쿄쿠쿼퀘퀴큐크킈키타태탸턔터테텨톄토톼퇘퇴툐투퉈퉤튀튜트틔티파패퍄퍠퍼페펴폐포퐈퐤푀표푸풔풰퓌퓨프픠피하해햐햬허헤혀혜호화홰회효후훠훼휘휴흐희히]?[ᅠ-ᆢ]+|[가-힣])[ᆨ-ᇹ]*|[ᄀ-ᅟ]+|[^\p{Cc}\p{Cf}\p{Zl}\p{Zp}])[\p{Mn}\p{Me}\x{09BE}\x{09D7}\x{0B3E}\x{0B57}\x{0BBE}\x{0BD7}\x{0CC2}\x{0CD5}\x{0CD6}\x{0D3E}\x{0D57}\x{0DCF}\x{0DDF}\x{200C}\x{200D}\x{1D165}\x{1D16E}-\x{1D172}]*|[\p{Cc}\p{Cf}\p{Zl}\p{Zp}])/u';
 
-	static function getGraphemeClusterArray($s)
+	static function &getGraphemeClusterArray($s)
 	{
 		preg_match_all(self::GRAPHEME_CLUSTER_RX, $s, $s);
 		return $s[0];
@@ -453,8 +453,9 @@ class u
 	protected static function rxClass($s, $class = '')
 	{
 		$class = array($class);
+		$s =& self::getGraphemeClusterArray($s);
 
-		foreach (self::getGraphemeClusterArray($s) as $s)
+		foreach ($s as $s)
 		{
 			if ('-' === $s) $class[0] = '-' . $class[0];
 			else if (strlen($s) <= 2) $class[0] .= preg_quote($s, '/');
