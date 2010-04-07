0d63fecefb3babea32681dac9eeaca6a639d1c94
Nicolas Grekas <nicolas.grekas@gmail.com>
2010-04-07 10:29:36 +0000

    * refactored class u around intl extension

--- class/u.php
+++ class/u.php
@@ -22,7 +22,7 @@
  */
 
 
-class u
+class u extends utf8_intl
 {
 	static function isUTF8($s)
 	{
@@ -94,7 +94,7 @@ class u
 	// Unicode transformation for caseless matching
 	// see http://unicode.org/reports/tr21/tr21-5.html
 
-	protected static $commonCaseFold  = array(
+	protected static $commonCaseFold = array(
 		'µ' => 'μ', 'ſ' => 's', "\xcd\x85" => 'ι', 'ς' => 'σ',
 		"\xcf\x90" => 'β', "\xcf\x91" => 'θ', "\xcf\x95" => 'φ',
 		"\xcf\x96" => 'π', "\xcf\xb0" => 'κ', "\xcf\xb1" => 'ρ',
@@ -126,31 +126,19 @@ class u
 
 	// Here is the complete set of native PHP string functions that need UTF-8 awareness
 
-	static function strlen($s)
-	{
-		preg_replace(self::GRAPHEME_CLUSTER_RX, '', $s, -1, $s);
-		return $s;
-	}
+	static function strlen($s) {return grapheme_strlen($s);}
+	static function substr($s, $start, $len = INF) {return INF === $len ? grapheme_substr($start, $len) : grapheme_substr($start, $len, $length);}
+	static function strpos  ($s, $needle, $offset = 0) {return grapheme_strpos  ($s, $needle, $offset);}
+	static function stripos ($s, $needle, $offset = 0) {return grapheme_stripos ($s, $needle, $offset);}
+	static function strrpos ($s, $needle, $offset = 0) {return grapheme_strrpos ($s, $needle, $offset);}
+	static function strripos($s, $needle, $offset = 0) {return grapheme_strripos($s, $needle, $offset);}
+	static function strstr  ($s, $needle, $before_needle = false) {return grapheme_strstr ($s, $needle, $before_needle);}
+	static function stristr ($s, $needle, $before_needle = false) {return grapheme_stristr($s, $needle, $before_needle);}
+	static function strrchr ($s, $needle, $before_needle = false) {return mb_strrchr ($s, $needle, $before_needle, 'UTF-8');}
+	static function strrichr($s, $needle, $before_needle = false) {return mb_strrichr($s, $needle, $before_needle, 'UTF-8');}
 
 	static function strtolower($s) {return mb_strtolower($s, 'UTF-8');}
 	static function strtoupper($s) {return mb_strtoupper($s, 'UTF-8');}
-
-	static function substr($s, $start, $len = INF)
-	{
-		$s =& self::getGraphemeClusterArray($s);
-		$s = array_slice($s, $start, INF === $len ? PHP_INT_MAX : $len);
-		return implode('', $s);
-	}
-
-	static function strpos  ($s, $needle, $offset = 0) {return self::position($s, $needle, $offset, 0);}
-	static function stripos ($s, $needle, $offset = 0) {return self::position($s, $needle, $offset, 1);}
-	static function strrpos ($s, $needle, $offset = 0) {return self::position($s, $needle, $offset, 2);}
-	static function strripos($s, $needle, $offset = 0) {return self::position($s, $needle, $offset, 3);}
-
-	static function stristr ($s, $needle, $before_needle = false) {return mb_stristr ($s, $needle, $before_needle, 'UTF-8');}
-	static function strrchr ($s, $needle, $before_needle = false) {return mb_strrchr ($s, $needle, $before_needle, 'UTF-8');}
-	static function strrichr($s, $needle, $before_needle = false) {return mb_strrichr($s, $needle, $before_needle, 'UTF-8');}
-	static function strstr  ($s, $needle, $before_needle = false) {return mb_strstr  ($s, $needle, $before_needle, 'UTF-8');}
 
 	static function htmlentities    ($s, $quote_style = ENT_COMPAT) {return htmlentities    ($s, $quote_style, 'UTF-8');}
 	static function htmlspecialchars($s, $quote_style = ENT_COMPAT) {return htmlspecialchars($s, $quote_style, 'UTF-8');}
@@ -179,7 +167,7 @@ class u
 			for ($j = 1; $j < $jLen; ++$j)
 			{
 				$w = $words[$j];
-				$wLen = self::strlen($w);
+				$wLen = grapheme_strlen($w);
 
 				if ($lineLen + $wLen < $width)
 				{
@@ -295,15 +283,15 @@ class u
 
 	static function str_pad($s, $len, $pad = ' ', $type = STR_PAD_RIGHT)
 	{
-		$slen = self::strlen($s);
+		$slen = grapheme_strlen($s);
 		if ($len <= $slen) return $s;
 
-		$padlen = self::strlen($pad);
+		$padlen = grapheme_strlen($pad);
 		$freelen = $len - $slen;
 		$len = $freelen % $padlen;
 
-		if (STR_PAD_RIGHT == $type) return $s . str_repeat($pad, $freelen / $padlen) . ($len ? self::substr($pad, 0, $len) : '');
-		if (STR_PAD_LEFT  == $type) return      str_repeat($pad, $freelen / $padlen) . ($len ? self::substr($pad, 0, $len) : '') . $s;
+		if (STR_PAD_RIGHT == $type) return $s . str_repeat($pad, $freelen / $padlen) . ($len ? grapheme_substr($pad, 0, $len) : '');
+		if (STR_PAD_LEFT  == $type) return      str_repeat($pad, $freelen / $padlen) . ($len ? grapheme_substr($pad, 0, $len) : '') . $s;
 
 		if (STR_PAD_BOTH == $type)
 		{
@@ -311,11 +299,11 @@ class u
 
 			$type = ceil($freelen);
 			$len = $type % $padlen;
-			$s  .=  str_repeat($pad, $type / $padlen) . ($len ? self::substr($pad, 0, $len) : '');
+			$s .= str_repeat($pad, $type / $padlen) . ($len ? grapheme_substr($pad, 0, $len) : '');
 
 			$type = floor($freelen);
 			$len = $type % $padlen;
-			return  str_repeat($pad, $type / $padlen) . ($len ? self::substr($pad, 0, $len) : '') . $s;
+			return str_repeat($pad, $type / $padlen) . ($len ? grapheme_substr($pad, 0, $len) : '') . $s;
 		}
 
 		trigger_error('u::str_pad(): Padding type has to be STR_PAD_LEFT, STR_PAD_RIGHT, or STR_PAD_BOTH.');
@@ -359,11 +347,11 @@ class u
 		if (1 == $format) for ($i = 1; $i < $len; $i+=2) $charlist[] = $s[$i];
 		else if (2 == $format)
 		{
-			$offset = self::strlen($s[0]);
+			$offset = grapheme_strlen($s[0]);
 			for ($i = 1; $i < $len; $i+=2)
 			{
 				$charlist[$offset] = $s[$i];
-				$offset += self::strlen($s[$i]) + self::strlen($s[$i+1]);
+				$offset += grapheme_strlen($s[$i]) + grapheme_strlen($s[$i+1]);
 			}
 		}
 		else $charlist = ($len - 1) / 2;
@@ -376,14 +364,14 @@ class u
 	static function strcasecmp   ($a, $b) {return self::strcmp   (self::strtocasefold($a), self::strtocasefold($b));}
 	static function strnatcasecmp($a, $b) {return self::strnatcmp(self::strtocasefold($a), self::strtocasefold($b));}
 	static function strncasecmp  ($a, $b, $len) {return self::strncmp(self::strtocasefold($a), self::strtocasefold($b), $len);}
-	static function strncmp      ($a, $b, $len) {return self::strcmp(self::substr($a, 0, $len), self::substr($b, 0, $len));}
+	static function strncmp      ($a, $b, $len) {return self::strcmp(grapheme_substr($a, 0, $len), grapheme_substr($b, 0, $len));}
 
 	static function strcspn($s, $charlist, $start = 0, $len = INF)
 	{
 		if ('' === (string) $mask) return null;
-		if ($start || INF !== $len) $s = self::substr($s, $start, $len);
+		if ($start || INF !== $len) $s = grapheme_substr($s, $start, $len);
 
-		return preg_match('/^(.*?)' . self::rxClass($mask) . '/us', $s, $len) ? self::strlen($len[1]) : self::strlen($s);
+		return preg_match('/^(.*?)' . self::rxClass($mask) . '/us', $s, $len) ? grapheme_strlen($len[1]) : grapheme_strlen($s);
 	}
 
 	static function strpbrk($s, $charlist)
@@ -399,8 +387,8 @@ class u
 
 	static function strspn($s, $mask, $start = INF, $len = INF)
 	{
-		if (INF !== $start || INF !== $len) $s = self::substr($s, $start, $len);
-		return preg_match('/^'  . self::rxClass($mask) . '+/u', $s, $s) ? self::strlen($s[0]) : 0;
+		if (INF !== $start || INF !== $len) $s = grapheme_substr($s, $start, $len);
+		return preg_match('/^' . self::rxClass($mask) . '+/u', $s, $s) ? grapheme_strlen($s[0]) : 0;
 	}
 
 	static function strtr($s, $from, $to = INF)
@@ -424,13 +412,13 @@ class u
 
 	static function substr_compare($a, $b, $offset, $len = INF, $i = 0)
 	{
-		$a = self::substr($a, $offset, $len);
+		$a = grapheme_substr($a, $offset, $len);
 		return $i ? self::strcasecmp($a, $b) : self::strcmp($a, $b);
 	}
 
 	static function substr_count($s, $needle, $offset = 0, $len = INF)
 	{
-		return substr_count(self::substr($s, $offset, $len), $needle);
+		return substr_count(grapheme_substr($s, $offset, $len), $needle);
 	}
 
 	static function substr_replace($s, $replace, $start, $len = INF)
@@ -456,43 +444,6 @@ class u
 		return mb_convert_case($s, MB_CASE_TITLE, 'UTF-8');
 	}
 
-
-	// (CRLF|([ZWNJ-ZWJ]|T+|L*(LV?V+|LV|LVT)T*|L+|[^Control])[Extend]*|[Control])
-	const GRAPHEME_CLUSTER_RX = '/(?:\r\n|(?:[ -~\x{200C}\x{200D}]|[ᆨ-ᇹ]+|[ᄀ-ᅟ]*(?:[가개갸걔거게겨계고과괘괴교구궈궤귀규그긔기까깨꺄꺠꺼께껴꼐꼬꽈꽤꾀꾜꾸꿔꿰뀌뀨끄끠끼나내냐냬너네녀녜노놔놰뇌뇨누눠눼뉘뉴느늬니다대댜댸더데뎌뎨도돠돼되됴두둬뒈뒤듀드듸디따때땨떄떠떼뗘뗴또똬뙈뙤뚀뚜뚸뛔뛰뜌뜨띄띠라래랴럐러레려례로롸뢔뢰료루뤄뤠뤼류르릐리마매먀먜머메며몌모뫄뫠뫼묘무뭐뭬뮈뮤므믜미바배뱌뱨버베벼볘보봐봬뵈뵤부붜붸뷔뷰브븨비빠빼뺘뺴뻐뻬뼈뼤뽀뽜뽸뾔뾰뿌뿨쀄쀠쀼쁘쁴삐사새샤섀서세셔셰소솨쇄쇠쇼수숴쉐쉬슈스싀시싸쌔쌰썌써쎄쎠쎼쏘쏴쐐쐬쑈쑤쒀쒜쒸쓔쓰씌씨아애야얘어에여예오와왜외요우워웨위유으의이자재쟈쟤저제져졔조좌좨죄죠주줘줴쥐쥬즈즤지짜째쨔쨰쩌쩨쪄쪠쪼쫘쫴쬐쬬쭈쭤쮀쮜쮸쯔쯰찌차채챠챼처체쳐쳬초촤쵀최쵸추춰췌취츄츠츼치카캐캬컈커케켜켸코콰쾌쾨쿄쿠쿼퀘퀴큐크킈키타태탸턔터테텨톄토톼퇘퇴툐투퉈퉤튀튜트틔티파패퍄퍠퍼페펴폐포퐈퐤푀표푸풔풰퓌퓨프픠피하해햐햬허헤혀혜호화홰회효후훠훼휘휴흐희히]?[ᅠ-ᆢ]+|[가-힣])[ᆨ-ᇹ]*|[ᄀ-ᅟ]+|[^\p{Cc}\p{Cf}\p{Zl}\p{Zp}])[\p{Mn}\p{Me}\x{09BE}\x{09D7}\x{0B3E}\x{0B57}\x{0BBE}\x{0BD7}\x{0CC2}\x{0CD5}\x{0CD6}\x{0D3E}\x{0D57}\x{0DCF}\x{0DDF}\x{200C}\x{200D}\x{1D165}\x{1D16E}-\x{1D172}]*|[\p{Cc}\p{Cf}\p{Zl}\p{Zp}])/u';
-
-	static function &getGraphemeClusterArray($s)
-	{
-		preg_match_all(self::GRAPHEME_CLUSTER_RX, $s, $s);
-		return $s[0];
-	}
-
-
-	protected static function position($s, $needle, $offset, $mode)
-	{
-		if (0 > $offset || ($offset && ('' === (string) $s || '' === $s = self::substr($s, $offset))))
-		{
-			trigger_error('Offset not contained in string.', E_USER_ERROR);
-			return false;
-		}
-
-		if ('' !== (string) $needle)
-		{
-			trigger_error('Empty delimiter.', E_USER_ERROR);
-			return false;
-		}
-
-		if ('' === (string) $s) return false;
-
-		switch ($mode)
-		{
-		case 0: $needle = iconv_strpos ($s, $needle, 0, 'UTF-8'); break;
-		case 1: $needle = mb_stripos   ($s, $needle, 0, 'UTF-8'); break;
-		case 2: $needle = iconv_strrpos($s, $needle,    'UTF-8'); break;
-		case 3: $needle = mb_strripos  ($s, $needle, 0, 'UTF-8'); break;
-		}
-
-		return $needle ? self::strlen(iconv_substr($s, 0, $needle, 'UTF-8')) + $offset : $needle;
-	}
 
 	protected static function natFold($s)
 	{
