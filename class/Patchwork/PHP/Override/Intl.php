cd4b6bc2dc02339489ffb05c076fe075e07c4d66
Nicolas Grekas <nicolas.grekas@gmail.com>
2011-04-12 20:22:35 +0200

    * make Patchwork\Utf8 use grapheme_extract() and implement this in Intl override

--- class/Patchwork/PHP/Override/Intl.php
+++ class/Patchwork/PHP/Override/Intl.php
@@ -17,6 +17,7 @@
  *
  * Implemented:
 
+grapheme_extract  - Extract a sequence of grapheme clusters from a text buffer, which must be encoded in UTF-8.
 grapheme_stripos  - Find position (in grapheme units) of first occurrence of a case-insensitive string
 grapheme_stristr  - Returns part of haystack string from the first occurrence of case-insensitive needle to the end of haystack.
 grapheme_strlen   - Get string length in grapheme units
@@ -31,30 +32,62 @@ grapheme_substr   - Return part of a string
 
 class Patchwork_PHP_Override_Intl
 {
-    static function strlen($s)
+    static function grapheme_extract($s, $size, $type = GRAPHEME_EXTR_COUNT, $start = 0, &$next = 0)
     {
-        preg_replace(Patchwork_Utf8::GRAPHEME_CLUSTER_RX, '', $s, -1, $s);
+        if (is_array($s)) return trigger_error(__METHOD__ . '() expects parameter 1 to be string, array given');
+
+        $s     = (string) $s;
+        $size  = (int) $size;
+        $type  = (int) $type;
+        $start = (int) $start;
+
+        if ('' === $s || 0 > $size || 0 > $start || 0 > $type || 2 < $type) return false;
+        if (0 === $size) return '';
+
+        $next = $start;
+        $s = substr($s, $start); //TODO: seek to the first character boundary when needed
+
+        if (GRAPHEME_EXTR_COUNT === $type)
+        {
+            $s = preg_split('/(' . self::GRAPHEME_CLUSTER_RX . "{1,{$size}})/u", $s, 2, PREG_SPLIT_DELIM_CAPTURE);
+            $next += strlen($s[0]);
+            $s = isset($s[1]) ? $s[1] : '';
+        }
+        else
+        {
+            //TODO
+            return trigger_error(__METHOD__ . '() with GRAPHEME_EXTR_MAXBYTES or GRAPHEME_EXTR_MAXCHARS is not implemented');
+        }
+
+        $next += strlen($s);
+
         return $s;
     }
 
-    static function substr($s, $start, $len = INF)
+    static function grapheme_strlen($s)
     {
-        preg_match_all(Patchwork_Utf8::GRAPHEME_CLUSTER_RX, $s, $s);
+        preg_replace('/' . self::GRAPHEME_CLUSTER_RX . '/u', '', $s, -1, $s);
+        return $s;
+    }
+
+    static function grapheme_substr($s, $start, $len = INF)
+    {
+        preg_match_all('/' . self::GRAPHEME_CLUSTER_RX . '/u', $s, $s);
         $s = array_slice($s[0], $start, INF === $len ? PHP_INT_MAX : $len);
         return implode('', $s);
     }
 
-    static function strpos  ($s, $needle, $offset = 0) {return self::position($s, $needle, $offset, 0);}
-    static function stripos ($s, $needle, $offset = 0) {return self::position($s, $needle, $offset, 1);}
-    static function strrpos ($s, $needle, $offset = 0) {return self::position($s, $needle, $offset, 2);}
-    static function strripos($s, $needle, $offset = 0) {return self::position($s, $needle, $offset, 3);}
-    static function stristr ($s, $needle, $before_needle = false) {return mb_stristr($s, $needle, $before_needle, 'UTF-8');}
-    static function strstr  ($s, $needle, $before_needle = false) {return mb_strstr ($s, $needle, $before_needle, 'UTF-8');}
+    static function grapheme_strpos  ($s, $needle, $offset = 0) {return self::grapheme_position($s, $needle, $offset, 0);}
+    static function grapheme_stripos ($s, $needle, $offset = 0) {return self::grapheme_position($s, $needle, $offset, 1);}
+    static function grapheme_strrpos ($s, $needle, $offset = 0) {return self::grapheme_position($s, $needle, $offset, 2);}
+    static function grapheme_strripos($s, $needle, $offset = 0) {return self::grapheme_position($s, $needle, $offset, 3);}
+    static function grapheme_stristr ($s, $needle, $before_needle = false) {return mb_stristr($s, $needle, $before_needle, 'UTF-8');}
+    static function grapheme_strstr  ($s, $needle, $before_needle = false) {return mb_strstr ($s, $needle, $before_needle, 'UTF-8');}
 
 
-    protected static function position($s, $needle, $offset, $mode)
+    protected static function grapheme_position($s, $needle, $offset, $mode)
     {
-        if (0 > $offset || ($offset && ('' === (string) $s || '' === $s = self::substr($s, $offset))))
+        if (0 > $offset || ($offset && ('' === (string) $s || '' === $s = self::grapheme_substr($s, $offset))))
         {
             trigger_error('Offset not contained in string.', E_USER_ERROR);
             return false;
@@ -76,18 +109,26 @@ class Patchwork_PHP_Override_Intl
         case 3: $needle = mb_strripos  ($s, $needle, 0, 'UTF-8'); break;
         }
 
-        return $needle ? self::strlen(iconv_substr($s, 0, $needle, 'UTF-8')) + $offset : $needle;
+        return $needle ? self::grapheme_strlen(iconv_substr($s, 0, $needle, 'UTF-8')) + $offset : $needle;
     }
+
+    // (CRLF|([ZWNJ-ZWJ]|T+|L*(LV?V+|LV|LVT)T*|L+|[^Control])[Extend]*|[Control])
+    const GRAPHEME_CLUSTER_RX = '(?:\r\n|(?:[ -~\x{200C}\x{200D}]|[ᆨ-ᇹ]+|[ᄀ-ᅟ]*(?:[가개갸걔거게겨계고과괘괴교구궈궤귀규그긔기까깨꺄꺠꺼께껴꼐꼬꽈꽤꾀꾜꾸꿔꿰뀌뀨끄끠끼나내냐냬너네녀녜노놔놰뇌뇨누눠눼뉘뉴느늬니다대댜댸더데뎌뎨도돠돼되됴두둬뒈뒤듀드듸디따때땨떄떠떼뗘뗴또똬뙈뙤뚀뚜뚸뛔뛰뜌뜨띄띠라래랴럐러레려례로롸뢔뢰료루뤄뤠뤼류르릐리마매먀먜머메며몌모뫄뫠뫼묘무뭐뭬뮈뮤므믜미바배뱌뱨버베벼볘보봐봬뵈뵤부붜붸뷔뷰브븨비빠빼뺘뺴뻐뻬뼈뼤뽀뽜뽸뾔뾰뿌뿨쀄쀠쀼쁘쁴삐사새샤섀서세셔셰소솨쇄쇠쇼수숴쉐쉬슈스싀시싸쌔쌰썌써쎄쎠쎼쏘쏴쐐쐬쑈쑤쒀쒜쒸쓔쓰씌씨아애야얘어에여예오와왜외요우워웨위유으의이자재쟈쟤저제져졔조좌좨죄죠주줘줴쥐쥬즈즤지짜째쨔쨰쩌쩨쪄쪠쪼쫘쫴쬐쬬쭈쭤쮀쮜쮸쯔쯰찌차채챠챼처체쳐쳬초촤쵀최쵸추춰췌취츄츠츼치카캐캬컈커케켜켸코콰쾌쾨쿄쿠쿼퀘퀴큐크킈키타태탸턔터테텨톄토톼퇘퇴툐투퉈퉤튀튜트틔티파패퍄퍠퍼페펴폐포퐈퐤푀표푸풔풰퓌퓨프픠피하해햐햬허헤혀혜호화홰회효후훠훼휘휴흐희히]?[ᅠ-ᆢ]+|[가-힣])[ᆨ-ᇹ]*|[ᄀ-ᅟ]+|[^\p{Cc}\p{Cf}\p{Zl}\p{Zp}])[\p{Mn}\p{Me}\x{09BE}\x{09D7}\x{0B3E}\x{0B57}\x{0BBE}\x{0BD7}\x{0CC2}\x{0CD5}\x{0CD6}\x{0D3E}\x{0D57}\x{0DCF}\x{0DDF}\x{200C}\x{200D}\x{1D165}\x{1D16E}-\x{1D172}]*|[\p{Cc}\p{Cf}\p{Zl}\p{Zp}])';
 }
 
 /**/if (!function_exists('grapheme_strlen'))
 /**/{
-        function grapheme_strlen  ($s) {return Patchwork_PHP_Override_Intl::strlen($s);}
-        function grapheme_strpos  ($s, $needle, $offset = 0) {return Patchwork_PHP_Override_Intl::strpos  ($s, $needle, $offset);}
-        function grapheme_stripos ($s, $needle, $offset = 0) {return Patchwork_PHP_Override_Intl::stripos ($s, $needle, $offset);}
-        function grapheme_strrpos ($s, $needle, $offset = 0) {return Patchwork_PHP_Override_Intl::strrpos ($s, $needle, $offset);}
-        function grapheme_strripos($s, $needle, $offset = 0) {return Patchwork_PHP_Override_Intl::strripos($s, $needle, $offset);}
-        function grapheme_stristr ($s, $needle, $before_needle = false) {return Patchwork_PHP_Override_Intl::stristr($s, $needle, $before_needle);}
-        function grapheme_strstr  ($s, $needle, $before_needle = false) {return Patchwork_PHP_Override_Intl::strstr ($s, $needle, $before_needle);}
-        function grapheme_substr  ($s, $start, $len = INF) {return Patchwork_PHP_Override_Intl::substr($s, $start, $len);}
+        define('GRAPHEME_EXTR_COUNT',    0);
+        define('GRAPHEME_EXTR_MAXBYTES', 1);
+        define('GRAPHEME_EXTR_MAXCHARS', 2);
+
+        function grapheme_extract ($s, $size, $type = GRAPHEME_EXTR_COUNT, $start = 0, &$next = 0) {return Patchwork_PHP_Override_Intl::grapheme_extract($s, $size, $type, $start, $next);}
+        function grapheme_strlen  ($s) {return Patchwork_PHP_Override_Intl::grapheme_strlen($s);}
+        function grapheme_strpos  ($s, $needle, $offset = 0) {return Patchwork_PHP_Override_Intl::grapheme_strpos  ($s, $needle, $offset);}
+        function grapheme_stripos ($s, $needle, $offset = 0) {return Patchwork_PHP_Override_Intl::grapheme_stripos ($s, $needle, $offset);}
+        function grapheme_strrpos ($s, $needle, $offset = 0) {return Patchwork_PHP_Override_Intl::grapheme_strrpos ($s, $needle, $offset);}
+        function grapheme_strripos($s, $needle, $offset = 0) {return Patchwork_PHP_Override_Intl::grapheme_strripos($s, $needle, $offset);}
+        function grapheme_stristr ($s, $needle, $before_needle = false) {return Patchwork_PHP_Override_Intl::grapheme_stristr($s, $needle, $before_needle);}
+        function grapheme_strstr  ($s, $needle, $before_needle = false) {return Patchwork_PHP_Override_Intl::grapheme_strstr ($s, $needle, $before_needle);}
+        function grapheme_substr  ($s, $start, $len = INF) {return Patchwork_PHP_Override_Intl::grapheme_substr($s, $start, $len);}
 /**/}
