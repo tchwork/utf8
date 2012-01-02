bcc508ab91f8490d147eb7504031a700410b1498
Nicolas Grekas <nicolas.grekas@gmail.com>
2012-01-02 17:52:47 +0100

    * move files around

--- pieces/agent-controller/class/u.php
+++
@@ -1,7 +0,0 @@
-<?php // vi: set fenc=utf-8 ts=4 sw=4 et:
-
-/**/if (DEBUG) user_error("Using class `u' for class `Patchwork\\Utf8' without declaring the alias with `use Patchwork\\Utf8 as u;' is deprecated", E_USER_DEPRECATED);
-
-class u extends Patchwork\Utf8
-{
-}
