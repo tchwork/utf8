1dca251ae0c1c853476e123f2fc7a0d8b02f5682
Nicolas Grekas <nicolas.grekas@gmail.com>
2012-01-28 15:14:03 +0100

    * replace assertEquals() by assertSame()

--- tests/Patchwork/Tests/PHP/Override/IntlTest.php
+++ tests/Patchwork/Tests/PHP/Override/IntlTest.php
@@ -8,16 +8,15 @@ class IntlTest extends \PHPUnit_Framework_TestCase
 {
     function testGrapheme_substr()
     {
-
         $c = "déjà";
 
-        $this->assertEquals( grapheme_substr($c, -2, 2), "jà" );
-        $this->assertFalse( grapheme_substr($c, -2, 3) );
+        $this->assertSame( i::grapheme_substr($c, -2, 2), "jà" );
+        $this->assertSame( i::grapheme_substr($c, -2, 3), "jà" );
 
-        $this->assertEquals( i::grapheme_substr($c, -2, 2), "jà" );
+        $this->assertSame( grapheme_substr($c, -2, 2), "jà" );
 
-        $this->markTestSkipped( "Skipping Intl::grapheme_substr() test with over limit length" );
+        $this->markTestSkipped( 'Skip testing grapheme_substr() with over limit $length parameter' );
 
-        $this->assertFalse( i::grapheme_substr($c, -2, 3) );
+        $this->assertSame( grapheme_substr($c, -2, 3), "jà" );
     }
 }
--- tests/Patchwork/Tests/Utf8Test.php
+++ tests/Patchwork/Tests/Utf8Test.php
@@ -37,49 +37,62 @@ class Utf8Test extends \PHPUnit_Framework_TestCase
 
     function testToASCII()
     {
-        $this->assertEquals( u::toASCII('déjà vu'), 'deja vu' );
+        $this->assertSame( u::toASCII('déjà vu'), 'deja vu' );
     }
 
     function testBestFit()
     {
-        $this->assertEquals( u::bestFit(1252, 'déjà vu'), iconv('UTF-8', 'CP1252', 'déjà vu') );
+        $this->assertSame( u::bestFit(1252, 'déjà vu'), iconv('UTF-8', 'CP1252', 'déjà vu') );
     }
 
     function testStrtocasefold()
     {
-        $this->assertEquals( u::strtocasefold('Σσς'), 'σσσ' );
+        $this->assertSame( u::strtocasefold('Σσς'), 'σσσ' );
     }
 
     function testStrtonatfold()
     {
-        $this->assertEquals( u::strtonatfold('Déjà Σσς'), 'Deja Σσς' );
+        $this->assertSame( u::strtonatfold('Déjà Σσς'), 'Deja Σσς' );
     }
 
     function testStrlen()
     {
         foreach (self::$utf8ValidityMap as $u => $t) if ($t)
         {
-            $this->assertEquals( u::strlen($u), 1 );
+            $this->assertSame( u::strlen($u), 1 );
         }
 
         $c = "déjà";
         $d = Normalizer::normalize("déjà", Normalizer::FORM_D);
         $this->assertTrue( $c > $d );
 
-        $this->assertEquals( u::strlen($c), 4 );
-        $this->assertEquals( u::strlen($c), u::strlen($d) );
+        $this->assertSame( u::strlen($c), 4 );
+        $this->assertSame( u::strlen($d), 4 );
     }
 
     function testSubstr()
     {
+        $b = "deja";
         $c = "déjà";
         $d = Normalizer::normalize("déjà", Normalizer::FORM_D);
         $this->assertTrue( $c > $d );
 
-        $this->assertEquals( u::substr($c, 0, 2), "dé" );
-        $this->assertEquals( Normalizer::normalize(u::substr($d, 0, 2), Normalizer::FORM_C), "dé" );
+        $this->assertSame( substr($b,  0,  2), "de" );
+        $this->assertSame( substr($b, -2,  3), "ja" );
+        $this->assertSame( substr($b, -3, -1), "ej" );
+        $this->assertSame( substr($b,  1, -3), "" );
+        $this->assertSame( substr($b,  1, -4), false );
 
-        $this->assertEquals( u::substr($c, -2), "jà");
-        $this->assertEquals( Normalizer::normalize(u::substr($d, -2), Normalizer::FORM_C), "jà" );
+        $this->assertSame( u::substr($c,  0,  2), "dé" );
+        $this->assertSame( u::substr($c, -2,  3), "jà" );
+        $this->assertSame( u::substr($c, -3, -1), "éj" );
+        $this->assertSame( u::substr($c,  1, -3), "" );
+        $this->assertSame( u::substr($c,  1, -4), false );
+
+        $this->assertSame( u::substr($d,  0,  2), Normalizer::normalize("dé", Normalizer::FORM_D) );
+        $this->assertSame( u::substr($d, -2,  3), Normalizer::normalize("jà", Normalizer::FORM_D) );
+        $this->assertSame( u::substr($d, -3, -1), Normalizer::normalize("éj", Normalizer::FORM_D) );
+        $this->assertSame( u::substr($d,  1, -3), "" );
+        $this->assertSame( u::substr($d,  1, -4), false );
     }
 }
