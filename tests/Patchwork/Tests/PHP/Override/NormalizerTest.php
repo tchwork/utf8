f9a04fbac79d019cf9a2328cb3fa4213f5bcdc1f
Nicolas Grekas <nicolas.grekas@gmail.com>
2012-04-06 20:13:28 +0200

    * more tests for Override\Normalizer

--- tests/Patchwork/Tests/PHP/Override/NormalizerTest.php
+++ tests/Patchwork/Tests/PHP/Override/NormalizerTest.php
@@ -10,8 +10,10 @@ class NormalizerTest extends \PHPUnit_Framework_TestCase
     function testIsNormalized()
     {
         $c = "déjà";
-        $d = in::normalize($c, in::NFD);
+        $d = in::normalize($c, pn::NFD);
 
+        $this->assertTrue( pn::isNormalized('abc') );
+        $this->assertTrue( pn::isNormalized($c) );
         $this->assertTrue( pn::isNormalized($c, pn::NFC) );
         $this->assertFalse( pn::isNormalized($d, pn::NFD) ); // The current implementation defensively says false
         $this->assertFalse( pn::isNormalized($c, pn::NFD) );
@@ -20,9 +22,19 @@ class NormalizerTest extends \PHPUnit_Framework_TestCase
 
     function testNormalize()
     {
-        $c = "déjà 훈쇼";
-        $d = in::normalize($c, in::NFD);
+        $c = in::normalize("déjà", pn::NFC) . in::normalize("훈쇼™", pn::NFD);
+        $this->assertSame( pn::normalize($c, pn::NONE), $c );
+        $this->assertSame( in::normalize($c, pn::NONE), $c );
+
+        $c = "déjà 훈쇼™";
+        $d = in::normalize($c, pn::NFD);
+        $kc = in::normalize($c, pn::NFKC);
+        $kd = in::normalize($c, pn::NFKD);
 
         $this->assertSame( pn::normalize($d), $c );
+        $this->assertSame( pn::normalize($d, pn::NFC), $c );
+        $this->assertSame( pn::normalize($c, pn::NFD), $d );
+        $this->assertSame( pn::normalize($d, pn::NFKC), $kc );
+        $this->assertSame( pn::normalize($c, pn::NFKD), $kd );
     }
 }
