bef8dbed9b9d4a3caa04d1a88c01c5139fe6ea95
Nicolas Grekas <nicolas.grekas@gmail.com>
2012-05-12 15:51:46 +0200

    + relicensed under APLv2+GPLv2

--- class/Patchwork/Utf8/Compiler.php
+++ class/Patchwork/Utf8/Compiler.php
@@ -1,15 +1,12 @@
-<?php /****************** vi: set fenc=utf-8 ts=4 sw=4 et: *****************
+<?php // vi: set fenc=utf-8 ts=4 sw=4 et:
+/*
+ * Copyright (C) 2012 Nicolas Grekas - p@tchwork.com
  *
- *   Copyright : (C) 2012 Nicolas Grekas. All rights reserved.
- *   Email     : p@tchwork.org
- *   License   : http://www.gnu.org/licenses/lgpl.txt GNU/LGPL
- *
- *   This library is free software; you can redistribute it and/or
- *   modify it under the terms of the GNU Lesser General Public
- *   License as published by the Free Software Foundation; either
- *   version 3 of the License, or (at your option) any later version.
- *
- ***************************************************************************/
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the (at your option):
+ * Apache License v2.0 (http://apache.org/licenses/LICENSE-2.0.txt), or
+ * GNU General Public License v2.0 (http://gnu.org/licenses/gpl-2.0.txt).
+ */
 
 namespace Patchwork\Utf8;
 
