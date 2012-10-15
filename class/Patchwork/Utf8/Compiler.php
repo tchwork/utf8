a58f8b3f854269f15c36742fab6561cfa38f8040
Nicolas Grekas <nicolas.grekas@gmail.com>
2012-10-15 15:03:37 +0200

    add bestfit handling

--- class/Patchwork/Utf8/Compiler.php
+++ class/Patchwork/Utf8/Compiler.php
@@ -16,6 +16,7 @@ namespace Patchwork\Utf8;
  *
  * See http://unicode.org/Public/UNIDATA/ for unicode data
  * See http://unicode.org/Public/MAPPINGS/ for charset conversion maps
+ * See http://unicode.org/Public/MAPPINGS/VENDORS/MICSFT/WindowsBestFit/ for mappings
  * See http://unicode.org/repos/cldr/trunk/common/transforms/ for Latin-ASCII.xml
  */
 class Compiler
@@ -135,6 +136,60 @@ class Compiler
         }
 
         file_put_contents($out_dir . 'translit.ser', serialize($map));
+    }
+
+    static function bestFit($out_dir, $map_dir = null)
+    {
+        isset($map_dir) || $map_dir = __DIR__ . '/unicode/charset/';
+        $dh = opendir($map_dir);
+        while (false !== $f = readdir($dh)) if (0 === strpos($f, 'bestfit') && preg_match('/^bestfit\d+\.txt$/D', $f))
+        {
+            $from = array();
+            $to = array();
+            $lead = '';
+
+            $h = fopen($map_dir . $f, 'rb');
+
+            while (false !== $s = fgets($h))
+            {
+                if (0 === strpos($s, 'WCTABLE')) break;
+                if (0 === strpos($s, 'DBCSTABLE'))
+                {
+                    $lead = substr(rtrim($s), -2);
+                    $lead = chr(hexdec($lead));
+                }
+                else if (preg_match("/^0x([0-9a-f]{2})\t0x([0-9a-f]{4})/", $s, $s))
+                {
+                    $s = array_map('hexdec', $s);
+                    $from[$lead . chr($s[1])] = self::chr($s[2]);
+                }
+            }
+
+            while (false !== $s = fgets($h))
+            {
+                if (0 === strpos($s, 'ENDCODEPAGE')) break;
+
+                $s = explode("\t", rtrim($s));
+
+                if (isset($s[1]))
+                {
+                    $s[0] = substr($s[0], 2);
+                    $s[1] = substr($s[1], 2);
+                    $s = array_map('hexdec', $s);
+                    $s[1] = $s[1] > 255
+                        ? chr($s[1]>>8) . chr($s[1]%256)
+                        : chr($s[1]);
+
+                    $to[self::chr($s[0])] = $s[1];
+                }
+            }
+
+            fclose($h);
+
+            file_put_contents($out_dir . 'from.' . substr($f, 0, -3) .'ser', serialize($from));
+            file_put_contents($out_dir . 'to.' . substr($f, 0, -3) .'ser', serialize($to));
+        }
+        closedir($dh);
     }
 
     // Write unicode data maps to disk
