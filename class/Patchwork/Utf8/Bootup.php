97a9b1e5a135aa63770f77c2a92b08fa0c37e972
Greg Roach <fisharebest@gmail.com>
2015-04-25 15:38:20 +0100

    iconv_set_encoding deprecated in PHP 5.6.0

--- class/Patchwork/Utf8/Bootup.php
+++ class/Patchwork/Utf8/Bootup.php
@@ -84,19 +84,25 @@ class Bootup
     {
         if (extension_loaded('iconv'))
         {
-            if ('UTF-8' !== iconv_get_encoding('input_encoding'))
+            if (PHP_VERSION_ID < 50600)
             {
-                iconv_set_encoding('input_encoding', 'UTF-8');
-            }
+                if ('UTF-8' !== iconv_get_encoding('input_encoding'))
+                {
+                    iconv_set_encoding('input_encoding', 'UTF-8');
+                }
 
-            if ('UTF-8' !== iconv_get_encoding('internal_encoding'))
-            {
-                iconv_set_encoding('internal_encoding', 'UTF-8');
-            }
+                if ('UTF-8' !== iconv_get_encoding('internal_encoding'))
+                {
+                    iconv_set_encoding('internal_encoding', 'UTF-8');
+                }
 
-            if ('UTF-8' !== iconv_get_encoding('output_encoding'))
-            {
-                iconv_set_encoding('output_encoding' , 'UTF-8');
+                if ('UTF-8' !== iconv_get_encoding('output_encoding'))
+                {
+                    iconv_set_encoding('output_encoding', 'UTF-8');
+                }
+            }
+            else {
+                ini_set('default_charset', 'UTF-8');
             }
         }
         else if (!defined('ICONV_IMPL'))
